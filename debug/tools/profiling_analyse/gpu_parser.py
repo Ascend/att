c5c52fec3ad77d04a643217e49dd1b4d06f78b08
zhouyanming <zhouyanming2@huawei.com>
2023-07-10 21:36:24 +0800

    fix e2e time

--- debug/tools/profiling_analyse/gpu_parser.py
+++ debug/tools/profiling_analyse/gpu_parser.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 import pandas as pd
 
 import parser_helper
@@ -7,6 +7,7 @@ import parser_helper
 class GpuProfilingParser:
     def __init__(self, args):
         self.trace_events = self.read_profiling_json_file(args.gpu)
+        self.compute_stream_id = self.infer_compute_stream_id()
         self.one_step_time = args.gpu_log_time
         self.profiling_info = parser_helper.ProfilingInfo()
 
@@ -22,12 +23,12 @@ class GpuProfilingParser:
         all_op_time = 0.0
         op_list = []
         compute_stream_dur = 0.0  # 计算流耗时
-        marks = defaultdict(int) # mark for compute communication_not_overlapped time
+        marks = defaultdict(int)  # mark for compute communication_not_overlapped time
 
         for event in self.trace_events:
             if not isinstance(event, dict):
                 continue
-            if event.get('args') and event.get('args').get('stream') == 7:
+            if event.get('args') and event.get('args').get('stream') == self.compute_stream_id:
                 compute_stream_dur += float(event.get('dur'))
             if not {'name', 'cat', 'dur', 'ts'} < event.keys():
                 continue
@@ -39,18 +40,19 @@ class GpuProfilingParser:
                 continue
             if 'nccl' in name:
                 for timestep in range(ts + 1, ts + dur + 1):
-                    marks[str(timestep)] += 1 # mark this timestep in communication stream
+                    marks[str(timestep)] += 1  # mark this timestep in communication stream
                 continue
             else:
                 for timestep in range(ts + 1, ts + dur + 1):
-                    marks[str(timestep)] += -100   # mark this timestep in compute stream
+                    marks[str(timestep)] += -100  # mark this timestep in compute stream
             if 'gemm' in name:
                 cube_time += float(dur)
             all_op_time += float(dur)
             op_list.append([ts, name, cat, dur])
         op_dataframe = pd.DataFrame(op_list, columns=['time start', 'name', 'cat', 'dur'])
         op_dataframe.to_csv('gpu_perf.csv', index=False)
-        self.profiling_info.communication_not_overlapped = len([_ for _,value in marks.items() if value > 0]) / 10 ** 6
+        self.profiling_info.compute_time = compute_stream_dur / 10 ** 6
+        self.profiling_info.communication_not_overlapped = len([_ for _, value in marks.items() if value > 0]) / 10 ** 6
         self.profiling_info.cube_time = cube_time / 10 ** 6
         self.profiling_info.vector_time = (all_op_time - cube_time) / 10 ** 6
         self.parse_e2e_time()
@@ -64,8 +66,13 @@ class GpuProfilingParser:
         self.parse_memory_reserved()
 
     def parse_e2e_time(self):
-        timeline = sorted(self.trace_events, key=lambda event: event.get('ts'))
-        self.profiling_info.e2e_time = (timeline[-1].get('ts') - timeline[0].get('ts')) / 10 ** 6
+        # timeline = sorted(self.trace_events, key=lambda event: event.get('ts'))
+        # self.profiling_info.e2e_time = (timeline[-1].get('ts') - timeline[0].get('ts')) / 10 ** 6
+        compute_events_timeline = [event for event in self.trace_events if
+                                   event.get('args') and event.get('args').get('stream') == self.compute_stream_id]
+        compute_events_timeline = sorted(compute_events_timeline, key=lambda event: event.get('ts'))
+        self.profiling_info.e2e_time = (compute_events_timeline[-1].get('ts') + compute_events_timeline[-1].get('dur') -
+                                        compute_events_timeline[0].get('ts')) / 10 ** 6
 
     def parse_memory_reserved(self):
         memories = [event.get('args').get('Total Reserved') for event in self.trace_events if
@@ -74,3 +81,12 @@ class GpuProfilingParser:
             print("Gpu profiling data doesn't contain memory info")
             return
         self.profiling_info.memory_used = max(memories) / 1024 ** 3
+
+    def infer_compute_stream_id(self):
+        kernel_stream_ids = [event.get('args').get('stream') for event in self.trace_events if
+                             event.get('cat') == 'Kernel' and 'nccl' not in event.get('name')
+                             and event.get('args') and event.get('args').get('stream')]
+        if not kernel_stream_ids:
+            raise RuntimeError('The profiling data does nor contain kernel running data.')
+        counter = Counter(kernel_stream_ids)
+        return counter.most_common(1)[0][0]
