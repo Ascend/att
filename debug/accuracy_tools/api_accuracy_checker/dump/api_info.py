c7a085e7f7b52d7349fa6857ef619eac52275a59
litian_drinksnow <litian68@huawei.com>
2023-07-31 20:43:17 +0800

    forward and backward api info class

--- debug/accuracy_tools/api_accuracy_checker/dump/api_info.py
+++ debug/accuracy_tools/api_accuracy_checker/dump/api_info.py
@@ -4,3 +4,34 @@
 class APIInfo:
     def __init__(self, api_name):
         self.api_name = api_name
+
+class ForwardAPIInfo(APIInfo):
+    def __init__(self, name, args, kwargs):
+        super().__init__(name)
+        self.analyze_api_input(args, kwargs)
+        self.analyze_api_call_stack() 
+    
+    def analyze_api_input(self, args, kwargs):
+        args_info_list = self.analyze_element(args)
+        kwargs_info_dict = self.analyze_element(kwargs)
+        self.api_info_struct = {self.api_name: {"args":args_info_list, "kwargs":kwargs_info_dict}}
+
+    def analyze_api_call_stack(self):
+        stack_str = []
+        for (_, path, line, func, code, _) in inspect.stack()[3:]:
+            stack_line = " ".join([
+                "File", ", ".join([path, " ".join(["line", str(line)]), " ".join(["in", func]),
+                                " ".join(["\n", code[0].strip() if code else code])])])
+            stack_str.append(stack_line)
+        self.stack_info_struct = {self.api_name: stack_str}
+    
+
+class BackwardAPIInfo(APIInfo):
+    def __init__(self, name, grads):
+        super().__init__(name)
+        self.analyze_api_input(grads)
+    
+    def analyze_api_input(self, grads):
+        grads_info_list = self.analyze_element(grads)
+        self.grad_info_struct = {self.api_name:grads_info_list}
+
