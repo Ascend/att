ac7a2299833a791eaa58b209e6fb710bf23a997e
sunyiming <sunyiming14@huawei.com>
2023-08-01 07:42:35 +0000

    update debug/accuracy_tools/api_accuracy_checker/dump/utils.py.
    Signed-off-by: sunyiming <sunyiming14@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/dump/utils.py
+++ debug/accuracy_tools/api_accuracy_checker/dump/utils.py
@@ -28,14 +28,6 @@ def write_npy(file_path, tensor):
     full_path = os.path.abspath(file_path+'.npy')
     return full_path
 
-def check_path(dump_path):
-    if not os.path.exists(dump_path):
-        raise ValueError(f"Path {dump_path} does not exist")
-    else:
-        current_permission = oct(os.stat(dump_path).st_mode)[-3:]
-        if current_permission != '750':
-            raise ValueError(f"Path {dump_path} does not have the required 0o750 permissions")
-
 class DumpUtil(object):
     save_real_data = False
     dump_path = './random_data_jsons'
