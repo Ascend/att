3f41d0734e2e420c37e2120f42fd698076bb04d7
sunyiming <sunyiming14@huawei.com>
2023-08-02 04:10:22 +0000

    update debug/accuracy_tools/api_accuracy_checker/dump/__init__.py.
    Signed-off-by: sunyiming <sunyiming14@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/dump/__init__.py
+++ debug/accuracy_tools/api_accuracy_checker/dump/__init__.py
@@ -2,9 +2,10 @@ from ..hook_module.register_hook import initialize_hook
 from .dump import pretest_hook
 from .info_dump import initialize_output_json
 from .utils import set_dump_switch
+from ..common.config import msCheckerConfig
 
 
 initialize_hook(pretest_hook)
 initialize_output_json()
 
-__all__ = ['set_dump_switch']
+__all__ = ['set_dump_switch', 'msCheckerConfig']
