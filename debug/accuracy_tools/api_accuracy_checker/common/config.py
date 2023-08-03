057312ef54cb217830c0e751963527e905281966
sunyiming <sunyiming14@huawei.com>
2023-08-03 01:27:33 +0000

    update debug/accuracy_tools/api_accuracy_checker/common/config.py.
    Signed-off-by: sunyiming <sunyiming14@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/common/config.py
+++ debug/accuracy_tools/api_accuracy_checker/common/config.py
@@ -31,4 +31,6 @@ class Config:
             else:
                 raise ValueError(f"Invalid key '{key}'")
 
-msCheckerConfig = Config('./config.yaml')
+cur_path = os.path.dirname(os.path.realpath(__file__))
+yaml_path = os.path.join(cur_path, "config.yaml")
+msCheckerConfig = Config(yaml_path)
