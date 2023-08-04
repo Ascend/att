db3844e2ecb2fe83ff0fe8450c88d9300ac7f0ad
h00613304 <hekunkun@huawei.com>
2023-08-04 16:14:33 +0800

    处理模型init阶段dump json报错的问题

--- debug/accuracy_tools/api_accuracy_checker/dump/__init__.py
+++ debug/accuracy_tools/api_accuracy_checker/dump/__init__.py
@@ -1,10 +1,7 @@
-from api_accuracy_checker.hook_module.register_hook import initialize_hook
-from api_accuracy_checker.dump.dump import pretest_hook
-from api_accuracy_checker.dump.info_dump import initialize_output_json
 from api_accuracy_checker.dump.utils import set_dump_switch
 
 
 initialize_hook(pretest_hook)
 initialize_output_json()
 
-__all__ = ['set_dump_switch', 'msCheckerConfig']
+__all__ = ['set_dump_switch']
