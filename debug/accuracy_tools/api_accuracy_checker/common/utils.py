160b0687ca37d598c6372e6c48ad4e40378f1776
jiangchangting1 <jiangchangting@huawei.com>
2023-08-07 09:24:10 +0000

    update debug/accuracy_tools/api_accuracy_checker/common/utils.py.
    Signed-off-by: jiangchangting1 <jiangchangting@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/common/utils.py
+++ debug/accuracy_tools/api_accuracy_checker/common/utils.py
@@ -83,7 +83,7 @@ class Const:
     }
 
     CONVERT_API = {
-        "fp16_to_fp32": ["conv2d", "batch_norm", "relu", "max_pool2d", "interpolate", "group_norm", "layer_norm", "bmm", "tanh"]
+        "fp16_to_fp32": ["conv2d", "batch_norm", "relu", "max_pool2d", "interpolate", "group_norm", "layer_norm", "bmm", "tanh",  "cross_entropy"]
     }
 
 class CompareConst:
