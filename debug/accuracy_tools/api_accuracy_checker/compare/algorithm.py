feca79474c771bb4239aaded7b90485375ce3e4e
litian_drinksnow <litian68@huawei.com>
2023-08-07 16:06:20 +0800

    fix typo

--- debug/accuracy_tools/api_accuracy_checker/compare/algorithm.py
+++ debug/accuracy_tools/api_accuracy_checker/compare/algorithm.py
@@ -6,9 +6,9 @@ from api_accuracy_checker.compare.compare_utils import CompareConst
 from api_accuracy_checker.common.utils import print_warn_log, Const
 
 def compare_torch_tensor(cpu_output, npu_output, compare_alg):
-    if cpu_output.dtype == torch.bool:
-        if npu_output.dtype != torch.bool:
-            return CompareConst.NAN, False, f"Bench out dtype is torch.bool but npu output dtype is {npu_output.dtype}, cannot compare."
+    if cpu_output.dtype == torch.bool or cpu_output.dtype == torch.uint8:
+        if npu_output.dtype != cpu_output.dtype:
+            return CompareConst.NAN, False, f"Bench out dtype is {cpu_output.dtype} but npu output dtype is {npu_output.dtype}, cannot compare."
         return compare_bool_tensor(cpu_output, npu_output)
     return compare_alg(cpu_output, npu_output)
 
@@ -57,29 +57,29 @@ def cosine_sim(cpu_output, npu_output):
     b_value = cpu_output.reshape(-1)
     cos = CompareConst.NA
     np.seterr(divide="ignore", invalid="ignore")
-    
+    if n_value.shape != b_value.shape:
+        msg = f"Shape of npu and bench outputs don't match. NPU: {n_value.shape}, bench: {b_value.shape}."
+        return  -1, False, msg
     if len(n_value) == 1:
-        print_warn_log("All the data in npu dump data is scalar. Compare by relative error.")
-        return get_max_rel_err(n_value, b_value)
-    if n_value.dtype == np.uint8:
-        return compare_uint8_data(n_value, b_value)
-    n_value = n_value / (np.max(np.abs(n_value)) + np.finfo(n_value.dtype).eps)
-    b_value = b_value / (np.max(np.abs(b_value)) + np.finfo(b_value.dtype).eps)
-    num = n_value.dot(b_value)
-    a_norm = np.linalg.norm(n_value)
-    b_norm = np.linalg.norm(b_value)
-    if a_norm <= np.finfo(float).eps and b_norm <= np.finfo(float).eps:
-        return cos, True
-    elif a_norm <= np.finfo(float).eps:
-        print_warn_log("All the data is Zero in npu dump data. Compare by relative error.")
-        return get_max_rel_err(n_value, b_value)
-    elif b_norm <= np.finfo(float).eps:
-        print_warn_log("All the data is Zero in bench dump data. Compare by relative error.")
+        msg = "All the data in npu dump data is scalar. Please refer to other compare algorithms."
+        return cos, True, msg 
+    n_value_max = np.max(np.abs(n_value))
+    b_value_max = np.max(np.abs(b_value))
+    if n_value_max <= np.finfo(float).eps and b_value_max <= np.finfo(float).eps:
+        return cos, True, msg 
+    elif n_value_max <= np.finfo(float).eps:
+        msg = "All the data is zero in npu dump data."
+        return CompareConst.NAN, False, msg 
+    elif b_value_max <= np.finfo(float).eps:
+        msg = "All the data is zero in bench dump data."
+        return CompareConst.NAN, False, msg 
     else:
-        cos = num / (a_norm * b_norm)
+        n_value /= n_value_max 
+        b_value /= b_value_max 
+        cos = np.dot(n_value, b_value) / (np.linalg.norm(n_value) * np.linalg.norm(b_value))
         if np.isnan(cos):
-            print_warn_log("Dump data has NaN when comparing with Cosine Similarity.")
-        return cos, cos > 0.99
+            msg = "Dump data has NaN when comparing with Cosine Similarity."
+        return cos, cos > 0.99, msg
 
 
 def compare_uint8_data(n_value, b_value):
@@ -90,9 +90,11 @@ def compare_uint8_data(n_value, b_value):
 
 
 def compare_builtin_type(bench_out, npu_out):
+    if not isinstance(bench_out, (bool, int, float, str)):
+        return CompareConst.NA, True, f"The data is not builtin type: {type(bench_out)}"
     if bench_out != npu_out:
-        return CompareConst.NAN, False
-    return 1.0, True
+        return CompareConst.NAN, False, ""
+    return True, True, ""
 
 
 def flatten_compare_result(result):
@@ -106,12 +108,13 @@ def flatten_compare_result(result):
 
 
 def compare_core(bench_out, npu_out, alg):
+    msg = ""
     if type(bench_out) != type(npu_out):
-        raise ValueError("bench and npu output type is different")
+        compare_result, test_success, msg = CompareConst.NAN, False, "bench and npu output type is different."
     if isinstance(bench_out, (list, tuple)):
         compare_result, test_success = [], True
         if len(bench_out) != len(npu_out):
-            raise ValueError("bench and npu output structure is different")
+            compare_result, test_success, msg = CompareConst.NAN, False, "bench and npu output structure is different"
         for b_out_i, n_out_i in zip(bench_out, npu_out):
             compare_result_i, test_success_i = compare_core(b_out_i, n_out_i, alg)
             compare_result.append(compare_result_i)
@@ -119,18 +122,20 @@ def compare_core(bench_out, npu_out, alg):
     elif isinstance(bench_out, dict):
         b_keys, n_keys = set(bench_out.keys()), set(npu_out.keys())
         if b_keys != n_keys:
-            raise ValueError("bench and npu output dictionary keys are different")
+            compare_result, test_success, msg = CompareConst.NAN, False, "bench and npu output dictionary keys are different"
         compare_result, test_success = compare_core(list(bench_out.values()), list(npu_out.values()))
     elif isinstance(bench_out, torch.Tensor):
-        compare_result, test_success = compare_torch_tensor(bench_out, npu_out, alg)
+        compare_result, test_success, msg = compare_torch_tensor(bench_out, npu_out, alg)
     elif isinstance(bench_out, (bool, int, float, str)):
-        compare_result, test_success = compare_builtin_type(bench_out, npu_out)
+        compare_result, test_success, msg = compare_builtin_type(bench_out, npu_out)
     elif bench_out is None:
-        return 1.0, True
+        compare_result, test_success, msg = CompareConst.NA, True, "output is None"
     else:
-        raise NotImplementedError("Unexpected output type in compare_core: {}".format(type(bench_out)))
+        compare_result, test_success, msg = CompareConst.NA, True, "Unexpected output type in compare_core: {}".format(type(bench_out))
     if isinstance(compare_result, list):
         compare_result = flatten_compare_result(compare_result)
+    else:
+        compare_result = [(compare_result, test_success, msg)]
     return compare_result, test_success
 
 
