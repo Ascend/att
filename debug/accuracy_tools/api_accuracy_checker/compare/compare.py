ab755df68ea00e0f8db8a70748b3615cb9ad66e4
jiangchangting1 <jiangchangting@huawei.com>
2023-08-22 07:36:52 +0000

    update debug/accuracy_tools/api_accuracy_checker/compare/compare.py.
    Signed-off-by: jiangchangting1 <jiangchangting@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/compare/compare.py
+++ debug/accuracy_tools/api_accuracy_checker/compare/compare.py
@@ -3,7 +3,7 @@ import os
 from prettytable import PrettyTable
 from api_accuracy_checker.compare.algorithm import compare_core, cosine_sim, cosine_standard, get_max_rel_err, \
     compare_builtin_type, get_rel_err_ratio_thousandth, get_rel_err_ratio_ten_thousandth
-from api_accuracy_checker.common.utils import get_json_contents, print_error_log, print_info_log, write_csv
+from api_accuracy_checker.common.utils import get_json_contents, print_error_log, print_info_log, write_csv, write_seg_csv
 from api_accuracy_checker.compare.compare_utils import CompareConst 
 
 
@@ -39,6 +39,7 @@ class Comparator:
             "forward_fail_num": 0, "backward_fail_num": 0, "forward_and_backward_fail_num": 0, "success_num": 0
         }
         self.result_save_path = result_save_path
+	self.write_csv_title()
 
     def print_pretest_result(self):
         res_dict = {
@@ -54,63 +55,41 @@ class Comparator:
         print_info_log(info_tb)
 
     def write_compare_csv(self):
-        self.write_summary_csv()
-        self.write_detail_csv()
+        self.write_summary_csv(self.test_results, self.save_path)
+        self.write_detail_csv(self.test_results, self.detail_save_path)
+        self.write_summary_csv(self.test_seg_results, self.seg_save_path)
+        self.write_detail_csv(self.test_seg_results, self.seg_detail_save_path)
 
-    def write_summary_csv(self):
-        test_rows = [[self.COLUMN_API_NAME, self.COLUMN_FORWARD_SUCCESS, self.COLUMN_BACKWARD_SUCCESS]]
-        if self.stack_info:
-            test_rows[0].append(self.COLUMN_STACK_INFO)
-        for result in self.test_results:
-            name = result[0]
-            df_row = list(result[:3])
-            if self.stack_info:
-                stack_info = "\n".join(self.stack_info[name])
-                df_row.append(stack_info)
-            test_rows.append(df_row)
-        write_csv(test_rows, self.save_path)
-
-    def write_detail_csv(self):
-        test_rows = [[
-            "Subject", "Bench Dtype", "NPU Dtype",
-            "Cosine Similarity", "Cosine Similarity Pass", "Cosine Similarity Message",
+    def write_csv_title(self):
+        summary_test_rows = [[self.COLUMN_API_NAME, self.COLUMN_FORWARD_SUCCESS, self.COLUMN_BACKWARD_SUCCESS]]
+        write_csv(summary_test_rows, self.save_path)
+        write_csv(summary_test_rows, self.seg_save_path)
+        detail_test_rows = [[
+            "Subject", "Cosine Similarity", "Cosine Similarity Pass", "Cosine Similarity Message",
             "Max Rel Error", "Max Rel Err Pass", "Max Rel Err Message",
-            "Thousandth Rel Error Ratio", "Thousandth Rel Error Ratio Pass", "Thousandth Rel Error Ratio Message",
-            "Ten Thousandth Rel Error Ratio", "Ten Thousandth Rel Error Ratio Pass", "Ten Thousandth Rel Error Ratio Message",
             "Compare Builtin Type", "Builtin Type Pass",
             "Builtin Type Message"
-        ]]  
-        for test_result in self.test_results:
-            subject_prefix = test_result[0]
-            fwd_result = test_result[3]
-            bwd_result = test_result[4]
-            if isinstance(fwd_result, list):
-                for i, test_subject in enumerate(fwd_result):
-                    subject = subject_prefix + ".forward.output." + str(i)
-                    test_rows.append([subject] + list(test_subject))
-            if isinstance(bwd_result, list):
-                for i, test_subject in enumerate(bwd_result):
-                    subject = subject_prefix + ".backward.output." + str(i)
-                    test_rows.append([subject] + list(test_subject))
+        ]]
+        write_csv(detail_test_rows, self.detail_save_path)
+        write_csv(detail_test_rows, self.seg_detail_save_path)
 
-        write_csv(test_rows, self.detail_save_path)
 
-    def write_seg_summary_csv(self):
+    def write_summary_csv(self, test_results, data_path):
         test_rows = []
         if self.stack_info:
             test_rows[0].append(self.COLUMN_STACK_INFO)
-        for result in self.test_seg_results:
+        for result in test_results:
             name = result[0]
             df_row = list(result[:3])
             if self.stack_info:
                 stack_info = "\n".join(self.stack_info[name])
                 df_row.append(stack_info)
             test_rows.append(df_row)
-        write_seg_csv(test_rows, self.seg_save_path)
+        write_seg_csv(test_rows, data_path)
 
-    def write_seg_detail_csv(self):
+    def write_detail_csv(self, test_results, data_path):
         test_rows = []
-        for test_result in self.test_seg_results:
+        for test_result in test_results:
             subject_prefix = test_result[0]
             fwd_result = test_result[3]
             bwd_result = test_result[4]
@@ -123,14 +102,14 @@ class Comparator:
                     subject = subject_prefix + ".backward.output." + str(i)
                     test_rows.append([subject] + list(test_subject))
 
-        write_seg_csv(test_rows, self.seg_detail_save_path)
+        write_seg_csv(test_rows, data_path)
 
     def record_results(self, *args):
         self.test_results.append(args)
         self.test_seg_results.append(args)
         if len(self.test_seg_results) == 100:
-            self.write_seg_summary_csv()
-            self.write_seg_detail_csv()
+            self.write_summary_csv(self.test_seg_results, self.seg_save_path)
+            self.write_detail_csv(self.test_seg_results, self.seg_detail_save_path)
             self.test_seg_results = []
 
     def register_compare_algorithm(self, name, compare_func, standard):
