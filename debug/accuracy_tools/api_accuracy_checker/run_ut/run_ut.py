ee2bd1ce8b7cfb21a6122a76a956f956ecf11e0f
l30044004 <louyujing@huawei.com>
2023-09-09 17:48:32 +0800

    csv文件api命名和dump数据命名不一致问题

--- debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
+++ debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
@@ -123,14 +123,11 @@ def do_save_error_data(api_full_name, data_info, is_fwd_success, is_bwd_success)
         api_full_name = api_full_name.replace("*", ".")
         for element in data_info.in_fwd_data_list:
             UtAPIInfo(api_full_name + '.forward.input', element)
-        if data_info.bench_out is not None:
-            UtAPIInfo(api_full_name + '.forward.output.bench', data_info.bench_out)
-            UtAPIInfo(api_full_name + '.forward.output.npu', data_info.npu_out)
-        if data_info.grad_in is not None:
-            UtAPIInfo(api_full_name + '.backward.input', data_info.grad_in)
-        if data_info.bench_grad_out is not None:
-            UtAPIInfo(api_full_name + '.backward.output.bench', data_info.bench_grad_out)
-            UtAPIInfo(api_full_name + '.backward.output.npu', data_info.npu_grad_out)
+        UtAPIInfo(api_full_name + '.forward.output.bench', data_info.bench_out)
+        UtAPIInfo(api_full_name + '.forward.output.npu', data_info.npu_out)
+        UtAPIInfo(api_full_name + '.backward.input', data_info.grad_in)
+        UtAPIInfo(api_full_name + '.backward.output.bench', data_info.bench_grad_out)
+        UtAPIInfo(api_full_name + '.backward.output.npu', data_info.npu_grad_out)
 
 
 
