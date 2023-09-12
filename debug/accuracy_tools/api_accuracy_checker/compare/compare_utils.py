023377dbf55ed24b422b25782318bccc9e0ef8f9
s30048155 <sunyiming14@huawei.com>
2023-09-12 16:51:21 +0800

    revert

--- debug/accuracy_tools/api_accuracy_checker/compare/compare_utils.py
+++ debug/accuracy_tools/api_accuracy_checker/compare/compare_utils.py
@@ -8,8 +8,17 @@ class CompareConst:
 
 
 def check_dtype_comparable(x, y):
-    dtype_set = {x.dtype, y.dtype}
-    if dtype_set.issubset(Const.FLOAT_TYPE) or dtype_set.issubset(Const.BOOL_TYPE) or dtype_set.issubset(Const.INT_TYPE):
-        return True
+    if x.dtype in Const.FLOAT_TYPE:
+        if y.dtype in Const.FLOAT_TYPE:
+            return True 
+        return False 
+    if x.dtype in Const.BOOL_TYPE:
+        if y.dtype in Const.BOOL_TYPE:
+            return True 
+        return False 
+    if x.dtype in Const.INT_TYPE:
+        if y.dtype in Const.INT_TYPE:
+            return True 
+        return False
     print_warn_log(f"Compare: Unexpected dtype {x.dtype}, {y.dtype}")
     return False 
