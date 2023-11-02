adc5fa78fcf867397ba9afc95de2f9d7736d37e1
s30048155 <sunyiming14@huawei.com>
2023-11-02 16:10:52 +0800

    add timestamp

--- debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
+++ debug/accuracy_tools/api_accuracy_checker/run_ut/run_ut.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import copy
 import sys
+import time
 import torch_npu
 import yaml
 import torch
@@ -213,7 +214,7 @@ def initialize_save_error_data():
     error_data_path_checker = FileChecker(msCheckerConfig.error_data_path, FileCheckConst.DIR,
                                           ability=FileCheckConst.WRITE_ABLE)
     error_data_path = error_data_path_checker.common_check()
-    initialize_save_path(error_data_path, 'ut_error_data' + str(os.getpid()))
+    initialize_save_path(error_data_path, 'ut_error_data' + time.strftime("%Y%m%d%H%M%S"))
 
 
 def _run_ut_parser(parser):
