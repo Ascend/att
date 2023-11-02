2f80648f68fe1a936ed7ed57367a70c43d7d59c6
sunyiming <sunyiming14@huawei.com>
2023-11-02 03:57:14 +0000

    update debug/accuracy_tools/api_accuracy_checker/common/base_api.py.
    Signed-off-by: sunyiming <sunyiming14@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/common/base_api.py
+++ debug/accuracy_tools/api_accuracy_checker/common/base_api.py
@@ -54,16 +54,13 @@ class BaseAPIInfo:
 
         else:
             api_args = self.api_name + '.' + str(self.args_num)
-            rank = self.get_tensor_rank(arg)
-            if rank is not None:
-                rank = "rank" + str(rank)
             if self.is_forward:
-                forward_real_data_path = os.path.join(self.save_path, self.forward_path, rank) if rank else os.path.join(self.save_path, self.forward_path)
+                forward_real_data_path = os.path.join(self.save_path, self.forward_path, self.rank)
                 if not os.path.exists(forward_real_data_path):
                     os.makedirs(forward_real_data_path)
                 file_path = os.path.join(forward_real_data_path, f'{api_args}.pt')
             else:
-                backward_real_data_path = os.path.join(self.save_path, self.backward_path, rank) if rank else os.path.join(self.save_path, self.backward_path)
+                backward_real_data_path = os.path.join(self.save_path, self.backward_path, self.rank)
                 if not os.path.exists(backward_real_data_path):
                     os.makedirs(backward_real_data_path)
                 file_path = os.path.join(backward_real_data_path, f'{api_args}.pt')
@@ -73,24 +70,6 @@ class BaseAPIInfo:
             single_arg.update({'datapath' : pt_path})
             single_arg.update({'requires_grad': arg.requires_grad})
         return single_arg
-
-    def get_tensor_rank(self, arg):
-        def get_tensor_rank_single(x):
-            if isinstance(x, (list, tuple)):
-                if len(x) > 0:
-                    return get_tensor_rank_single(x[0])
-                return None
-            elif isinstance(x, torch.Tensor):
-                device = x.device
-                if device.type == 'cpu':
-                    return None
-                else:
-                    return device.index
-            return None
-        rank = get_tensor_rank_single(arg)
-        if rank is None:
-            return None
-        return rank
 
     def analyze_builtin(self, arg):
         single_arg = {}
