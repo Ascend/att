026b16002e7fea688046ce2ce78c7fe3bf26c550
jiangchangting1 <jiangchangting@huawei.com>
2023-12-04 11:39:41 +0000

    update debug/accuracy_tools/api_accuracy_checker/hook_module/wrap_torch.py.
    Signed-off-by: jiangchangting1 <jiangchangting@huawei.com>

--- debug/accuracy_tools/api_accuracy_checker/hook_module/wrap_torch.py
+++ debug/accuracy_tools/api_accuracy_checker/hook_module/wrap_torch.py
@@ -23,13 +23,8 @@ import yaml
 from api_accuracy_checker.hook_module.hook_module import HOOKModule
 from api_accuracy_checker.common.utils import torch_device_guard
 from api_accuracy_checker.common.config import msCheckerConfig
-
+from api_accuracy_checker.hook_module.utils import WrapTorchOps
 from ptdbg_ascend.src.python.ptdbg_ascend.common.file_check_util import FileOpen
-
-cur_path = os.path.dirname(os.path.realpath(__file__))
-yaml_path = os.path.join(cur_path, "support_wrap_ops.yaml")
-with FileOpen(yaml_path, 'r') as f:
-    WrapTorchOps = yaml.safe_load(f).get('torch')
 
 
 def get_torch_ops():
